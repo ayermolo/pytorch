c911fff7ec34e5dbfa1b39bf2f4368917ff36e8d
Wanchao Liang <wanchaol@users.noreply.github.com>
2023-04-10 22:36:22 +0000

    [spmd] move the param_buffers to the front
    Makes it a bit easy to track the parameter count and corresponding named
    states
    
    ghstack-source-id: c98d5cbca07c245dc5392569a2174e2480e06560
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/98437

--- torch/distributed/_spmd/api.py
+++ torch/distributed/_spmd/api.py
@@ -187,10 +187,10 @@ def _override_placements(t: torch.Tensor, placements: List[Placement]):
 
 def _dtensor_expand(
     gm: fx.GraphModule,
+    params_and_buffers: Dict[str, Any],
+    named_states: Dict[str, Any],
     args: Tuple[Any, ...],
     kwargs: Dict[str, Any],
-    named_states: Dict[str, Any],
-    params_and_buffers: Dict[str, Any],
 ) -> fx.GraphModule:
     flat_args, _ = pytree.tree_flatten(list(args) + list(kwargs.values()))
 
@@ -201,6 +201,11 @@ def _dtensor_expand(
 
     inps, schemas = [], []
 
+    for p in pytree.tree_flatten(params_and_buffers)[0]:
+        assert isinstance(p, torch.Tensor), f"expecting Tensor but got {type(p)}"
+        inps.append(p)
+        schemas.append(replicate_schema)
+
     for o in pytree.tree_flatten(named_states)[0]:
         if isinstance(o, torch.Tensor):
             inps.append(o)
@@ -208,11 +213,6 @@ def _dtensor_expand(
         else:
             inps.append(torch.empty(0))
             schemas.append(replicate_schema)
-
-    for p in pytree.tree_flatten(params_and_buffers)[0]:
-        assert isinstance(p, torch.Tensor), f"expecting Tensor but got {type(p)}"
-        inps.append(p)
-        schemas.append(replicate_schema)
 
     for a in flat_args:
         if isinstance(a, torch.Tensor):
@@ -471,7 +471,7 @@ def _compile(
 
     # Lift states and parameters as function arguments so that make_fx
     # can trace operations applied to them.
-    def stateless_func(func, named_states, params_and_buffers, args, kwargs):
+    def stateless_func(func, params_and_buffers, named_states, args, kwargs):
         with stateless._reparametrize_module(
             cast(nn.Module, mod), params_and_buffers
         ), _rematerialize_optimizer(
@@ -495,10 +495,10 @@ def _compile(
             tracing_mode="symbolic",
             decomposition_table=FOREACH_DECOMP_TABLE,
             _allow_non_fake_inputs=False,
-        )(named_states, params_and_buffers, args, kwargs)
+        )(params_and_buffers, named_states, args, kwargs)
 
     # 4. Use DTensor to insert collectives
-    gm = _dtensor_expand(gm, args, kwargs, named_states, params_and_buffers)
+    gm = _dtensor_expand(gm, params_and_buffers, named_states, args, kwargs)
 
     # 5. Move the responsibility of flattening the input arguments from the
     # graph module to the caller. This serves two purposes:
@@ -506,7 +506,7 @@ def _compile(
     #   container that maintains the state tensors in the same order as they
     #   appear in graph placeholders.
     #   - Reduced runtime cost. The state container is only flattened once upfront.
-    flat_state, _ = pytree.tree_flatten([named_states, params_and_buffers])
+    flat_state, _ = pytree.tree_flatten([params_and_buffers, named_states])
     gm = _to_caller_flattened_graph_module(gm)
 
     # 6. dedup comm operators.
